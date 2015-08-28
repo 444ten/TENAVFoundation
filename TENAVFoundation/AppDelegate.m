593edfabfd01ea370b4e141b12c97d248f6839fa
444ten <444ten@gmail.com>
2015-08-28 14:00:48 +0300

    project -> initial commit

---
+++ TENAVFoundation/AppDelegate.m
@@ -0,0 +1,33 @@
+//
+//  AppDelegate.m
+//  TENAVFoundation
+//
+//  Created by 444ten on 8/28/15.
+//  Copyright (c) 2015 444ten. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+#import "TENStartViewController.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    UIWindow *window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+
+    TENStartViewController *controller = [TENStartViewController new];
+    window.rootViewController = controller;
+    
+    [window makeKeyAndVisible];
+
+    self.window = window;
+    
+    return YES;
+}
+
+@end
---
+++ TENAVFoundation/Source/GUI/TENStartViewController/TENStartViewController.h
@@ -0,0 +1,13 @@
+//
+//  TENStartViewController.h
+//  TENAVFoundation
+//
+//  Created by 444ten on 8/28/15.
+//  Copyright (c) 2015 444ten. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TENStartViewController : UIViewController
+
+@end
