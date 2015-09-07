0a4dc70b052b68c99cdb2e82211cad464398c60a
444ten <444ten@gmail.com>
2015-09-07 14:40:06 +0300

    TENLPFilter -> initial commit

---
+++ TENAVFoundation/Source/DSP/TENLPFilter/TENLPFilter.h
@@ -0,0 +1,13 @@
+//
+//  TENLPFilter.h
+//  TENAVFoundation
+//
+//  Created by 444ten on 9/7/15.
+//  Copyright (c) 2015 444ten. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TENLPFilter : NSObject
+
+@end
---
+++ TENAVFoundation/Source/DSP/TENLPFilter/TENLPFilter.mm
@@ -0,0 +1,13 @@
+//
+//  TENLPFilter.m
+//  TENAVFoundation
+//
+//  Created by 444ten on 9/7/15.
+//  Copyright (c) 2015 444ten. All rights reserved.
+//
+
+#import "TENLPFilter.h"
+
+@implementation TENLPFilter
+
+@end
