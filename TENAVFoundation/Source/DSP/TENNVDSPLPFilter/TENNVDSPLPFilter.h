19501405519d5fa38c2e63759ffa6bcc8d67d449
444ten <444ten@gmail.com>
2015-09-08 15:49:11 +0300

    TENNVDSPLPFilter -> initial commit

---
+++ TENAVFoundation/Source/DSP/TENNVDSPLPFilter/TENNVDSPLPFilter.h
@@ -0,0 +1,13 @@
+//
+//  TENNVDSPLPFilter.h
+//  TENAVFoundation
+//
+//  Created by 444ten on 9/8/15.
+//  Copyright (c) 2015 444ten. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TENNVDSPLPFilter : NSObject
+
+@end
---
+++ TENAVFoundation/Source/DSP/TENNVDSPLPFilter/TENNVDSPLPFilter.m
@@ -0,0 +1,13 @@
+//
+//  TENNVDSPLPFilter.m
+//  TENAVFoundation
+//
+//  Created by 444ten on 9/8/15.
+//  Copyright (c) 2015 444ten. All rights reserved.
+//
+
+#import "TENNVDSPLPFilter.h"
+
+@implementation TENNVDSPLPFilter
+
+@end
