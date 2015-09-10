8c90767ffedfd1f8b0d926b5cf1cf8f29c030c98
444ten <444ten@gmail.com>
2015-09-10 10:42:49 +0300

    TENSuperPoweredEQFilter -> initial commit

---
+++ TENAVFoundation/Source/DSP/TENSuperPoweredEQFilter/TENSuperPoweredEQFilter.m
@@ -0,0 +1,13 @@
+//
+//  TENSuperPoweredEQFilter.m
+//  TENAVFoundation
+//
+//  Created by 444ten on 9/10/15.
+//  Copyright (c) 2015 444ten. All rights reserved.
+//
+
+#import "TENSuperPoweredEQFilter.h"
+
+@implementation TENSuperPoweredEQFilter
+
+@end
