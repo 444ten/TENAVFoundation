4dfc7208fee0e3d5378d186cd0afb17b5962a887
444ten <444ten@gmail.com>
2015-09-10 12:32:55 +0300

    TENSuperPoweredEchoFilter -> initial commit

---
+++ TENAVFoundation/Source/DSP/TENSuperPoweredEchoFilter/TENSuperPoweredEchoFilter.m
@@ -0,0 +1,13 @@
+//
+//  TENSuperPoweredEchoFilter.m
+//  TENAVFoundation
+//
+//  Created by 444ten on 9/10/15.
+//  Copyright (c) 2015 444ten. All rights reserved.
+//
+
+#import "TENSuperPoweredEchoFilter.h"
+
+@implementation TENSuperPoweredEchoFilter
+
+@end
